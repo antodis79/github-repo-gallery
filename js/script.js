ba4f50ae86f9ad54bf8b7a357d95d24fd406adbd
anto_dis <disty79.ads@gmail.com>
2022-07-01 17:47:13 +0200

    Repo list added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const profileInfo = document.querySelector(".overview");
 /*** profile info will appear here ***/
 const username = "antodis79";
+const repoList = document.querySelector(".repo-list");
+
+
 
 const getUserInfo = async function () {
     const userInfo = await fetch(`https://api.github.com/users/${username}`
@@ -25,4 +28,20 @@ const displayUserInfo = function (data) {
     <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
   </div>`;
   profileInfo.append(newDiv);
+  getRepo();
+};
+
+const getRepo = async function () {
+  const res = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+  const repoData = await res.json();
+  displayRepoInfo(repoData);
+};
+
+const displayRepoInfo = function (repos) {
+  for (const repo of repos) {
+    const repoItem = document.createElement("li");
+    repoItem.classList.add("repo");
+    repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+    repoList.append(repoItem);
+  }
 };
